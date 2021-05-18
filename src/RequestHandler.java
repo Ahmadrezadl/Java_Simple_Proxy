901cb9a2e1afc63dbd7e6c63f734bf03b757233c
Ahmadreza <46781518+Ahmadrezadl@users.noreply.github.com>
2021-05-19 01:58:22 +0430

    handle and forward get and post requests (finally i did it =) )

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class RequestHandler implements Runnable{
     private final Socket socket;
@@ -38,16 +42,59 @@ public class RequestHandler implements Runnable{
             e.printStackTrace();
         }
     }
-
-    private void handleRequest() {
+//http://ahmadrezadownload.loxblog.com/
+    private void handleRequest()  {
         try {
             URL url = new URL(fullUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod(method);
-            for (int i = 0; i < 5; i++) {
-                System.out.println(reader.readLine());
+            connection.setDoOutput(true);
+//            var bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//            for(int i = 0; i <10;i++)
+//            System.out.println(bf.readLine());
+            int contentLength = 0;
+            while (true) {
+                String s = reader.readLine();
+                System.out.println(s);
+                if(s.equals(""))break;
+                String key = s.split(":",2)[0];
+                String value = s.split(":",2)[1].substring(1);
+                connection.setRequestProperty(key,value);
+                if(key.equals("Content-Length"))
+                    contentLength = Integer.parseInt(value);
+
             }
-        } catch (MalformedURLException e) {
+            for (int i = 0; i < contentLength; i++) {
+                connection.getOutputStream().write(reader.read());
+            }
+            connection.getOutputStream().flush();
+            System.out.println("ANSWERING...");
+            String line = connection.getHeaderField(null) + "\r\n";
+            for(var entry : connection.getHeaderFields().entrySet())
+            {
+                if(entry.getKey() == null)continue;
+                String value = "";
+                for(String s : entry.getValue())
+                    value += s+",";
+                value = value.substring(0,value.length()-1);
+                line += entry.getKey() + ": " + value + "\r\n";
+            }
+            line+="\r\n";
+            byte [] res = connection.getInputStream().readAllBytes();
+            line+= res.length + "\r\n";
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            baos.write(line.getBytes());
+            baos.write(res);
+
+            System.out.println(baos.toString());
+            socket.getOutputStream().write(baos.toByteArray());
+
+
+            socket.getOutputStream().flush();
+            socket.getOutputStream().close();
+        }
+        catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
@@ -56,6 +103,7 @@ public class RequestHandler implements Runnable{
 
 
     private void handleConnect() throws IOException {
+        System.out.println(fullUrl);
         String [] urlAndPort = fullUrl.replaceFirst("htt((p)|(ps))://" , "").split(":");
         String url = urlAndPort[0];
         int port = Integer.parseInt(urlAndPort[1]);
